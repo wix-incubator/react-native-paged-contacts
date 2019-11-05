5c70ec25ead8562742fe44cb28b0a1fe75927976
Ran Greenberg <gran33@gmail.com>
2019-11-05 18:01:11 +0200

    upgrade iOS native

--- ios/RCTPagedContacts/RCTPagedContactsModule.h
+++ ios/RCTPagedContacts/RCTPagedContactsModule.h
@@ -1,8 +1,4 @@
-#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
-#else
-#import "RCTBridgeModule.h"
-#endif
 
 @interface RCTPagedContactsModule : NSObject <RCTBridgeModule>
 
